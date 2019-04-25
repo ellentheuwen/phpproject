2bb38e66273165cb4ff9e2ad2550ae32d1c0e8a5
Ellen <ellen.theuwen@gmail.com>
2019-04-25 12:17:35 +0200

    sql

--- sql/netflix.sql
+++ sql/netflix.sql
@@ -1,68 +1,57 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 5.5.55-0ubuntu0.14.04.1)
-# Database: netflix
-# Generation Time: 2018-04-10 08:11:13 +0000
-# ************************************************************
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Gegenereerd op: 25 apr 2019 om 10:17
+-- Serverversie: 5.7.23
+-- PHP-versie: 7.2.10
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+--
+-- Database: `netflix`
+--
 
+-- --------------------------------------------------------
 
-# Dump of table collection
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `collection`;
-
-CREATE TABLE `collection` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `poster` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-LOCK TABLES `collection` WRITE;
-/*!40000 ALTER TABLE `collection` DISABLE KEYS */;
-
-INSERT INTO `collection` (`id`, `title`, `poster`)
-VALUES
-	(1,'Blade Runner 2046','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaoJ20qUVNCzeZnQ7Aq2aBiBdpcJQMG4_u2JhNXpvL4mLXwO2H'),
-	(2,'Black Mirror','http://www.sickchirpse.com/wp-content/uploads/2017/10/Black-Mirror.jpg'),
-	(3,'Stranger Things','https://www.comicbookmovie.com/images/articles/banners/154707.jpg');
-
-/*!40000 ALTER TABLE `collection` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-# Dump of table users
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `users`;
+--
+-- Tabelstructuur voor tabel `users`
+--
 
 CREATE TABLE `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `users`
+--
 
+INSERT INTO `users` (`id`, `email`, `password`) VALUES
+(1, 'test@test.com', '$2y$10$mg7bGn4MkWOOHaEr1oNBXuEe/HH4Nb/l14/b.mIhbiGWDEMTDWgSa'),
+(2, 'test@test.com', '$2y$10$WDnzMOZyY5W4Te4Wdy9D6eWBRHu9lV39BY0rrn2B9DhaBEb.fENnS'),
+(3, 'ellen.theuwen@gmail.com', '$2y$10$vi8dgDWC58WR4lbjvnNGOuJCn0XqfWHizVDf88i/tZILKND7sRBRq'),
+(4, 'test@test.com', '$2y$10$Rlc4WxAhL777sqgi8DLSQ.X8wV8UBmIuDAKf0g3yi9gfox7XVbVMq');
 
+--
+-- Indexen voor geëxporteerde tabellen
+--
 
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Indexen voor tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
