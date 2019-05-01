5f9ed4348ce6f58495a436c0eaa5b93c851cb2eb
Ellen <ellen.theuwen@gmail.com>
2019-05-01 18:08:31 +0200

    sql update settings

--- sql/talktype.sql
+++ sql/talktype.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Gegenereerd op: 30 apr 2019 om 16:19
+-- Gegenereerd op: 01 mei 2019 om 16:08
 -- Serverversie: 5.7.23
 -- PHP-versie: 7.2.10
 
@@ -37,32 +37,6 @@ CREATE TABLE `posts` (
 INSERT INTO `posts` (`id`, `description`, `picture`, `hashtags`, `location`, `likes`, `time`) VALUES
 (1, 'Dit is een voorbeeld', 'https://www.business-punk.com/wp-content/uploads/2018/04/logo2-400x400.jpg', '#voorbeeld #logo', 'Mechelen', 0, '2019-04-30 18:18:04');
 
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `firstname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Gegevens worden geëxporteerd voor tabel `users`
---
-
-INSERT INTO `users` (`id`, `email`, `password`, `username`, `firstname`, `lastname`) VALUES
-(22, 'test2@test.com', '$2y$10$PaLBsSJl.lz8f5VdXFRe2uwLgjwpTIY.2cz2VIfMRMUidXgDPhcbe', 'tester3', 'test2', 'test2'),
-(23, 'test2@test.com', '$2y$10$PaLBsSJl.lz8f5VdXFRe2uwLgjwpTIY.2cz2VIfMRMUidXgDPhcbe', 'tester3', 'test2', 'test2'),
-(24, 'test@test.com', '$2y$10$ux93WJ0oy44uDt/N632BvOLCebem3VFIeYYL/Wm/OwjIuiMdxCFTK', 'tester', 'test', 'test'),
-(25, 'test@php.com', '$2y$10$Jq2n5Fg.vcxEVUH1aZWBf.J4ZPyVRn18UwH4M6xViTqKEe0bX5aqy', 'tester', 'test2', 'test2'),
-(26, 'test@test.com', '$2y$10$Ype07kepdlgzBZcyQ5Rin.x8nYIWGWJYBwHKfNnRMse4nxVke9gk.', 'test', 'test', 'test');
-
 --
 -- Indexen voor geëxporteerde tabellen
 --
@@ -74,12 +48,6 @@ ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexen voor tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT voor geëxporteerde tabellen
 --
 
@@ -88,9 +56,3 @@ ALTER TABLE `users`
 --
 ALTER TABLE `posts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT voor een tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
