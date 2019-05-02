9c7ba69a45bfb8c5dd03739f6d32e7722fe4d3f1
Ellen <ellen.theuwen@gmail.com>
2019-05-02 11:27:51 +0200

    css + description post

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
   session_start();
-  include_once("classes/Item.class.php");
-  include_once("classes/User.class.php");
+  include_once 'classes/Item.class.php';
+  include_once 'classes/User.class.php';
 
   User::checkLogin();
 
-  $posts= Item::getAll();
-  
+  $posts = Item::getAll();
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,17 +18,23 @@
 <body>
   
   <div id="talktype">
-  <?php include_once("nav.inc.php"); ?>
+  <?php include_once 'nav.inc.php'; ?>
   
   <div class="feed">
-    <?php foreach($posts as $p): ?>
-    <a href="details.php?watch=<?php echo $p['id']; ?>" class="feed__posts" style="background-image: url(<?php echo $p['picture']; ?>)">
-    </a>
-    <?php endforeach; ?>
+
+    <?php foreach ($posts as $p): ?>
+    <a href="details.php?id=<?php echo $p['id']; ?>" > <img class="feed__posts"src="<?php echo $p['picture']; ?>" alt="Post"></a>   
+    <div class="info_posts"> 
+        <p class="postinfo"><?php echo $p['location']; ?></p> 
+        <p class="postinfo"><?php echo $p['description']; ?> <?php echo $p['hashtags']; ?></p>
+        <p class="postinfo">Total likes: <?php echo $p['likes']; ?></p>
+<?php endforeach; ?>
+
+    </div>
   </div>
 </div>
 
-<button id="load--more">Load More</button>
+<button class="load_more">Let's load some more really nice posts</button>
 
 </body>
 </html>
