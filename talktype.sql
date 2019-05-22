48e68d1d9a86e179201dda3c183f1eb97a751959
Ellen <ellen.theuwen@gmail.com>
2019-05-22 19:59:11 +0200

    sql

--- talktype.sql
+++ talktype.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Gegenereerd op: 15 mei 2019 om 17:32
+-- Gegenereerd op: 22 mei 2019 om 17:58
 -- Serverversie: 5.7.23
 -- PHP-versie: 7.2.10
 
@@ -32,32 +32,11 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`id`, `text`, `user_id`, `post_id`) VALUES
-(1, 'Hallloooo', 1, 1),
 (2, 'heykes', 1, 83),
 (3, 'Leavin\' a really cool comment', 1, 104),
-(4, 'Leavin\' a really cool comment', 1, 104);
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `followers`
---
-
-CREATE TABLE `followers` (
-  `id` int(11) NOT NULL,
-  `user_id1` int(11) NOT NULL,
-  `user_id2` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Gegevens worden geëxporteerd voor tabel `followers`
---
-
-INSERT INTO `followers` (`id`, `user_id1`, `user_id2`) VALUES
-(1, 23, 28),
-(3, 26, 28),
-(4, 28, 23),
-(5, 26, 23);
+(5, 'test', 1, 106),
+(29, 'cool man', 1, 98),
+(30, 'Is it done already?', 1, 109);
 
 -- --------------------------------------------------------
 
@@ -78,17 +57,17 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `image`, `description`, `user_id`, `date`) VALUES
-(89, 'images/post_images/09fdf0ceb0e75c1f132ac276c6c47f2d.jpg', 'get it done babyyyyy', 41, '2019-05-14 20:13:34'),
 (90, 'images/post_images/aac96010bd5922c448b1b8fe2b3acbd3.jpg', 'on the edge of glory, lady gaga would say', 41, '2019-05-14 20:13:55'),
 (91, 'images/post_images/85b60216b0862325e1d190f254ab5933.jpg', 'php alllll the way', 41, '2019-05-14 20:15:02'),
 (92, 'images/post_images/ed190ff44b1b3b639bb3c4f1e68c9301.jpg', 'abcdefuckit', 41, '2019-05-14 20:18:11'),
 (93, 'images/post_images/562c6ec5f8161be9ce92a3cb78fc6428.jpg', 'maar nie de voice van vlaanderen hé', 41, '2019-05-14 20:18:30'),
 (95, 'images/post_images/0296d6985ec2238e643482767c847fc2.jpg', 'php makes me lonely', 41, '2019-05-14 20:36:16'),
 (97, 'images/post_images/9a3e42f0f74161fd3ee8606e64bb5c84.jpg', 'pink poster with a lot of text', 41, '2019-05-14 20:43:22'),
-(98, 'images/post_images/ab00d17046fa9b0f1ab1da86305db2a2.jpg', 'you have gotta seek is yourself', 41, '2019-05-14 20:44:21'),
-(99, 'images/post_images/9eeba1bd89ddc62dfd40498787365743.jpg', 'school is a game', 41, '2019-05-14 20:44:39'),
-(100, 'images/post_images/5275255ee622d0da8b3aed89abd63049.jpg', 'but more features would be nice', 41, '2019-05-14 20:45:50'),
-(106, 'images/post_images/88d22259fd48d6fa744a9e236fcabe91.jpg', 'wish i was there', 41, '2019-05-15 17:32:14');
+(98, 'images/post_images/ab00d17046fa9b0f1ab1da86305db2a2.jpg', 'you gotta seek it yourself', 41, '2019-05-20 18:10:26'),
+(99, 'images/post_images/9eeba1bd89ddc62dfd40498787365743.jpg', 'school is a game', 41, '2019-05-19 20:44:39'),
+(100, 'images/post_images/5275255ee622d0da8b3aed89abd63049.jpg', 'but more features would be nice', 41, '2019-05-21 20:45:50'),
+(106, 'images/post_images/88d22259fd48d6fa744a9e236fcabe91.jpg', 'wish i was there', 41, '2019-05-15 17:32:14'),
+(109, 'images/post_images/09fdf0ceb0e75c1f132ac276c6c47f2d.jpg', 'get it done girl!', 41, '2019-05-22 15:36:38');
 
 -- --------------------------------------------------------
 
@@ -112,9 +91,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `firstname`, `lastname`, `username`, `password`, `bio`, `avatar`) VALUES
-(26, 'test@test.be', 'test', 'test', 'test', '$2y$12$143VKfRWHLVAOnSSMcOdguWiIvlgnpYr56RqeZM8mupIK6u8aZuLe', '', 'images/profile_images/download.jpeg'),
-(40, 'test@test.com', 'test', 'test', 'tester1', '$2y$12$Fc.IK18J71zWPjAkexnE7uLv9WvNxn6n5JBxA5k7L48i5c37CdbSa', '', 'images/profile_images/59649478_2320141851645531_1153026735600566272_n.jpg'),
-(41, 'ellen.theuwen@gmail.com', 'ellen', 'theuwen', 'ellentriek', '$2y$12$JlFEBJMetOosIWgpnPVIyel/8E53VtoNJQslbUjxv0vOYwTMyGBsS', 'dit is een bio', 'images/profile_images/59649478_2320141851645531_1153026735600566272_n.jpg');
+(41, 'ellen.theuwen@gmail.com', 'ellen', 'theuwen', 'ellentriek', '$2y$12$JlFEBJMetOosIWgpnPVIyel/8E53VtoNJQslbUjxv0vOYwTMyGBsS', 'hupla', ''),
+(48, 'test@test.com', 'test', 'test', 'test', '$2y$12$PiO75QntnYnjSVaqUmWId.Bv85d5y0uvdikoZPPn5EvjVWd/QkbV2', '', 'images/profile_images/59649478_2320141851645531_1153026735600566272_n.jpg'),
+(49, 'php@test.com', 'php', 'php', 'php', '$2y$12$wjKVBTYiUMlGULcNWPzBguQd48GqeCL4ceHiooz6EqeBxcSPOAwRu', '', '');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -124,12 +103,6 @@ INSERT INTO `users` (`id`, `email`, `firstname`, `lastname`, `username`, `passwo
 -- Indexen voor tabel `comments`
 --
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexen voor tabel `followers`
---
-ALTER TABLE `followers`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -152,22 +125,16 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT voor een tabel `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT voor een tabel `followers`
---
-ALTER TABLE `followers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT voor een tabel `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=115;
 
 --
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
